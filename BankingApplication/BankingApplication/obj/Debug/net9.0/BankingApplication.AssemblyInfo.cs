f6489403a7d5f50175c338004c0d95d53d750996
s3965206 <s3965206@student.rmit.edu.au>
2025-10-02 12:11:12 +1000

    files cant be unstaged but doesn't affect code

--- BankingApplication/BankingApplication/obj/Debug/net9.0/BankingApplication.AssemblyInfo.cs
+++ BankingApplication/BankingApplication/obj/Debug/net9.0/BankingApplication.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BankingApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f8a518cfcfaded550930b6d894c602a5ca594ca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dce6996f66fe0bc22694c88992b942bc91781f37")]
 [assembly: System.Reflection.AssemblyProductAttribute("BankingApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BankingApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
