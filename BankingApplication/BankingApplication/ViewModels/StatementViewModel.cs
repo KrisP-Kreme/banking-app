1c48c43744460d4beff3827e0a208026b8f3602e
s3965206 <s3965206@student.rmit.edu.au>
2025-10-06 17:24:26 +1100

    statement page

---
+++ BankingApplication/BankingApplication/ViewModels/StatementViewModel.cs
@@ -0,0 +1,13 @@
+﻿using BankingApplication.Models;
+
+namespace BankingApplication.ViewModels;
+
+public class StatementViewModel
+{
+    public int AccountNumber { get; set; }
+    public Account Account { get; set; }
+    public List<Transaction> Transactions { get; set; }
+    public int CurrentPage { get; set; }
+    public int TotalPages { get; set; }
+}
+
